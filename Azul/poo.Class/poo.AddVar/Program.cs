912afb7b2a76f6aa4a7a20e4ffff8b4de02c5827
Christian Pineda <viajero06701@innovaccion.mx>
2022-07-21 14:52:47 -0500

    Actualización Azul Class AddVar

---
+++ Azul/poo.Class/poo.AddVar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace poo.AddVar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el radio del círculo");
+
+            AreaCirculo area = new AreaCirculo();
+
+            double radio = area.Area(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("El area del círculo es: {0}", radio);
+            Console.ReadKey();
+        }
+    }
+
+    public class AreaCirculo
+    {
+        public double Area(double radio)
+        {
+            return Math.PI * Math.Pow(radio, 2);
+        }
+    }
+}
