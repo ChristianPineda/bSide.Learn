49f8fda52e2fc83a2465d5b722ba1eeb2a411c73
Christian Pineda <viajero06701@innovaccion.mx>
2022-08-12 00:09:26 -0500

    Modificación de proyecto BaseSchool

--- Azul/poo.Heredity/poo.BaseSchool/Program.cs
+++ Azul/poo.Heredity/poo.BaseSchool/Program.cs
@@ -6,7 +6,56 @@ namespace poo.BaseSchool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Matematicas materia1 = new Matematicas("Ema", 59);
+            materia1.GetInfoMateria();
+
+            Musica materia2 = new Musica("Manuel", 54);
+            materia2.GetInfoMateria();
+
+            Lenguaje materia3 = new Lenguaje("Mary", 44);
+            materia3.GetInfoMateria();
+        }
+    }
+
+    class Materia
+    {
+        private string name;
+        private string teacher;
+        private int students;
+
+        public Materia(string name, string teacher, int students)
+        {
+            this.name = name;
+            this.teacher = teacher;
+            this.students = students;
+        }
+
+        public void GetInfoMateria()
+        {
+            Console.WriteLine("La materia: "+this.name+" impartida por "+this.teacher+" tiene "+this.students+" estudiantes");
+        }
+    }
+    class Matematicas:Materia
+    {
+        public Matematicas(string teacher, int students) : base("Matemáticas", teacher, students)
+        {
+                
+        }
+    }
+
+    class Musica : Materia
+    {
+        public Musica(string teacher, int students) : base("Música", teacher, students)
+        {
+                
+        }
+    }
+
+    class Lenguaje : Materia
+    {
+        public Lenguaje(string teacher, int students) : base("Lenguaje", teacher, students)
+        {
+                
         }
     }
 }
