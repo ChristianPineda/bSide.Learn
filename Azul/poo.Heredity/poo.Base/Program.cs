67489c7de439669cdaa40f2db210d2603aa2c01a
Christian Pineda <viajero06701@innovaccion.mx>
2022-08-11 22:41:44 -0500

    Modificación de proyecto Base

--- Azul/poo.Heredity/poo.Base/Program.cs
+++ Azul/poo.Heredity/poo.Base/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace poo.Base
 {
@@ -6,7 +7,67 @@ namespace poo.Base
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tecnico tecnico = new Tecnico("Ema","Rodhes", 001);
+            tecnico.GetInfoEmpleado();
+
+            Secretaria secretaria = new Secretaria("Noemi", "Soto", 002);
+            secretaria.GetInfoEmpleado();
+
+            Conserje conserje = new Conserje("Pedro", "Molina", 003);
+            conserje.GetInfoEmpleado();
+            conserje.GetHashCode();
+        }
+    }
+
+    class Empleado
+    {
+        private string nombre;
+        private string apellido;
+        private int clave;
+
+        public Empleado(string nombre, string apellido, int clave)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.clave = clave;
+        }
+        public void GetInfoEmpleado()
+        {
+            Console.WriteLine("La clave del empleado "+ this.nombre + " " + this.apellido+" es: "+ this.clave);
+        }
+    }
+
+    class Tecnico : Empleado
+    {
+        public Tecnico(string nombre, string apellido, int clave) : base(nombre, apellido, clave)
+        {
+
+        }
+        public void Analizar()
+        {
+            Console.WriteLine("El técnico está analizando");
+        }
+    }
+    class Secretaria : Empleado
+    {
+        public Secretaria(string nombre, string apellido, int clave) : base(nombre, apellido, clave)
+        {
+            
+        }
+        public void Archivar()
+        {
+            Console.WriteLine("La secretaria está archivando");
+        }
+    }
+    class Conserje : Empleado
+    {
+        public Conserje(string nombre, string apellido, int clave) : base(nombre, apellido, clave)
+        {
+            
+        }
+        public void Barrer()
+        {
+            Console.WriteLine("El conserje está barriendo");
         }
     }
 }
