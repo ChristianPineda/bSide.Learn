e94b4beb4fd41351cf46ae48d86f974bd21a7b42
Christian Pineda Berruecos <cpineda@bside.com.mx>
2022-09-09 09:57:21 -0500

    Creación de proyecto Recipes

---
+++ Azul/poo.Interfaces/poo.Recipes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace poo.Recipes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
