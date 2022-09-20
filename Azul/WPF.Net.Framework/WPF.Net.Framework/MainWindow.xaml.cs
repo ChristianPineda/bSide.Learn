b01d623ee7d77d9f8a430ed861b0d5874ad4be40
Christian Pineda <viajero06701@innovaccion.mx>
2022-09-20 00:43:15 -0500

    Cambio de paleta de colores y se agregó un botón sin función

--- Azul/WPF.Net.Framework/WPF.Net.Framework/MainWindow.xaml.cs
+++ Azul/WPF.Net.Framework/WPF.Net.Framework/MainWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace WPF.Net.Framework
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
     }
 }
