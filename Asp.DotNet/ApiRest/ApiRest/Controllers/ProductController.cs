a25352ed0fd43de7c6760845340d924e0367fa34
Christian Pineda <viajero06701@innovaccion.mx>
2022-10-07 13:58:03 -0500

    Obtiene una lista de productos y por Id

---
+++ Asp.DotNet/ApiRest/ApiRest/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using ApiRest.Model;
+using ApiRest.Model.Repo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ApiRest.Controllers
+{
+    [Route("products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly MemoryProduct repo;
+
+        public ProductController()
+        {
+            repo = new MemoryProduct();
+        }
+        
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            var listProducts = repo.GetAll();
+            return listProducts;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = repo.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+    }
+}
