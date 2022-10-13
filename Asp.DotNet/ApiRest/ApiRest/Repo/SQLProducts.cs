be494678df2a6a09b286573fdf16eb9f040d20eb
Christian Pineda <viajero06701@innovaccion.mx>
2022-10-12 23:32:46 -0500

    GetById

--- Asp.DotNet/ApiRest/ApiRest/Repo/SQLProducts.cs
+++ Asp.DotNet/ApiRest/ApiRest/Repo/SQLProducts.cs
@@ -32,7 +32,7 @@ namespace ApiRest.Repo
                 comm.Parameters.Add("@description", SqlDbType.VarChar, 200).Value = p.Description;
                 comm.Parameters.Add("@price", SqlDbType.Float).Value = p.Price;
                 comm.Parameters.Add("@sku", SqlDbType.VarChar, 50).Value = p.Sku;
-                comm.ExecuteNonQuery();
+                comm.ExecuteNonQuery(); //No queries
             }
             catch (Exception e)
             {
@@ -58,7 +58,42 @@ namespace ApiRest.Repo
 
         public Product GetById(string code)
         {
-            throw new NotImplementedException();
+            SqlConnection sqlConnection = GetConnection();
+            SqlCommand comm = null;
+            
+            Product p = null;
+            try
+            {
+                sqlConnection.Open();
+                comm = sqlConnection.CreateCommand();
+                comm.CommandText = "dbo.GetProducts";
+                comm.CommandType = CommandType.StoredProcedure;
+                comm.Parameters.Add("@sku", SqlDbType.VarChar, 50).Value = code;
+                SqlDataReader reader = comm.ExecuteReader(); //Read queries
+                
+                if (reader.Read())
+                {
+                    p = new Product
+                    {
+                        Name = reader["Name"].ToString(),
+                        Description = reader["Description"].ToString(),
+                        Price = (decimal) Convert.ToDouble(reader["Price"].ToString()),
+                        Sku = reader["SKU"].ToString(),
+                    };
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al crear el producto" + e);
+            }
+            finally
+            {
+                comm?.Dispose();
+                sqlConnection.Close();
+                sqlConnection.Dispose();
+            }
+
+            return p;
         }
 
         public void ModifyProduct(Product p)
