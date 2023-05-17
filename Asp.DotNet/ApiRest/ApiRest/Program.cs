404396ae690bf0594b1f26ad98ac9b6a37624778
Christian Pineda <viajero06701@innovaccion.mx>
2023-05-16 18:53:38 -0600

    RazorPagesPizza Project Add

--- Asp.DotNet/ApiRest/ApiRest/Program.cs
+++ Asp.DotNet/ApiRest/ApiRest/Program.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using NLog.Extensions.Logging; // Aventar los logs a un txt
 namespace ApiRest
 {
     public class Program
